b9fd299a25abe6fba254cf77afccaa7068585c04
Enrique Ramirez <nrqrmz@gmail.com>
2024-06-10 03:00:56 -0500

    add about and testimonies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,31 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: %i[home about testimonies]
 
   def home
+    @bom = book_of_mormon
+  end
+
+  def about
+    @bom = book_of_mormon
+  end
+
+  def testimonies
+    @bom = book_of_mormon
+    @bom_data = word_counter(@bom['testimonies'][0]['text'])
+    @bom_data2 = word_counter(@bom['testimonies'][1]['text'])
+  end
+
+  private
+
+  def book_of_mormon
     require 'open-uri'
     json = File.join(__dir__, "../../book_of_mormon.json")
-    api = JSON.parse(File.read(json))
-    @bom = api
+    JSON.parse(File.read(json))
+  end
+
+  def word_counter(text)
+    stop_words = %w[the and of to in a that it is was i for he you as his with be not are this but they on have at by she or an we do from which one would all will there say who make when can more if unto our also us]
+
+    text.tr('.,;:?!', '').split.map(&:downcase).reject { |w| stop_words.include? w }.tally.sort_by { |_, v| -v }.to_h
   end
 end
