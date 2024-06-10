6fac97e51544ff46f88b5276f805a21863b23629
Enrique Ramirez <nrqrmz@gmail.com>
2024-06-10 02:11:13 -0500

    upload json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: :home
 
   def home
+    require 'open-uri'
+    json = File.join(__dir__, "../../book_of_mormon.json")
+    api = JSON.parse(File.read(json))
+    @bom = api
   end
 end
